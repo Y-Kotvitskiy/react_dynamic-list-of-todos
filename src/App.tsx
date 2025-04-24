e5ef558a6df19cfbd09789d6e7acbee79f878f7d
Евгений Котвицкий <ekotvizky@gmail.com>
2025-04-24 14:26:24 +0300

    load todos from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,18 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [todos, setTodos] = React.useState<Todo[]>([]);
+
+  useEffect(() => {
+    getTodos().then(setTodos);
+    setIsLoading(false);
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -21,14 +31,13 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading ? <Loader /> : <TodoList todos={todos} />}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {false ? <TodoModal /> : null}
     </>
   );
 };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Todo } from '../../types/Todo';
 
-export const TodoList: React.FC = () => (
+interface TodoListProps {
+  todos: Todo[];
+}
+
+export const TodoList: React.FC<TodoListProps> = ({ todos }) => (
   <table className="table is-narrow is-fullwidth">
     <thead>
       <tr>
@@ -14,7 +19,24 @@ export const TodoList: React.FC = () => (
         <th> </th>
       </tr>
     </thead>
-
+    <tbody>
+      {todos.map(todo => (
+        <tr key={todo.id} data-cy="todo" className="">
+          <td className="is-vcentered">{todo.id}</td>
+          <td className="is-vcentered" />
+          <td className="is-vcentered is-expanded">
+            <p className="has-text-danger">delectus aut autem</p>
+          </td>
+          <td className="has-text-right is-vcentered">
+            <button data-cy="selectButton" className="button" type="button">
+              <span className="icon">
+                <i className="far fa-eye" />
+              </span>
+            </button>
+          </td>
+        </tr>
+      ))}
+    </tbody>
     <tbody>
       <tr data-cy="todo" className="">
         <td className="is-vcentered">1</td>
